835717573ec6db5d0bfaae2eadb78322f24985f0
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2019-11-12 17:26:48 +0100

    from spring start

---
+++ src/main/java/pl/dk/jcrplay/JcrPlayApplication.java
@@ -0,0 +1,12 @@
+package pl.dk.jcrplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcrPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcrPlayApplication.class, args);
+	}
+}
